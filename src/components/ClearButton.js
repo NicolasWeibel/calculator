4399371d91edaa671ed19dcc3ec2f97abe9879fa
NicolasWeibel <123994887+NicolasWeibel@users.noreply.github.com>
2023-09-30 03:21:15 -0300

    Add ClearButton component

---
+++ src/components/ClearButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../stylesheets/ClearButton.css'
+
+const ClearButton = () => {
+  return (
+    <div className='clear-button'>
+      Clear
+    </div>
+  )
+}
+
+export default ClearButton
